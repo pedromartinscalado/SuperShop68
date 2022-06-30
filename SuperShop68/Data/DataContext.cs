73c98d22c16fa59651806939216260b5a3d0e7c8
Pedro Calado <Pedro.Martins.Calado@formandos.cinel.pt>
2022-06-30 22:33:36 +0100

    First controller

---
+++ SuperShop68/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SuperShop68.Data.Entities;
+
+namespace SuperShop68.Data
+{
+    public class DataContext : DbContext
+    {
+
+        // propriedade que fica ligada ao products
+        public DbSet<Product> Products { get; set; }
+
+
+
+        // injeto a minha própria DataContext que criei
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+    }
+}
