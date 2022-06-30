0567f48f881ccd2b7e0d794847fb3a2dcbb98fd3
Pedro Calado <Pedro.Martins.Calado@formandos.cinel.pt>
2022-06-30 20:53:43 +0100

    Add project files.

---
+++ SuperShop68/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SuperShop68.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
